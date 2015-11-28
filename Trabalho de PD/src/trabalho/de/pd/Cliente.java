970fe781b6769a2c93c1918b6636861fef343db8
zrogan <Carlos Oliveira@Workspace>
2015-11-28 20:35:56 +0000

    PedeFicheiro em processo

--- Trabalho de PD/src/trabalho/de/pd/Cliente.java
+++ Trabalho de PD/src/trabalho/de/pd/Cliente.java
@@ -7,13 +7,19 @@ package trabalho.de.pd;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
@@ -24,6 +30,8 @@ import java.util.Scanner;
 public class Cliente {
     final static String HOSTNAME_DIRETORIA = "225.15.15.15";
     final static int PORT_DIRETORIA = 7000;
+    final static int MAX_SIZE = 4000;
+    final static int TIMEOUT = 5;
     
     Socket servidorPrincipal = null;
     ClienteInfo clienteInfo = null;
@@ -82,9 +90,105 @@ public class Cliente {
         }
     }
     
-    public void pedeFicheiro() {
+    public void pedeFicheiro(String fileToGet,String stringLocalDirectory) {
+        File localDirectory;
+        String fileName, localFilePath = null;
+        FileOutputStream localFileOutputStream = null;
+        int serverPort;
+        Socket socketToServer = null;
+        PrintWriter pout;
+        InputStream in;
+        byte []fileChunck = new byte[MAX_SIZE];
+        int nbytes;
+        /*
+        if(args.length != 4){
+            System.out.println("Sintaxe: java GetFileTcpClient serverAddress serverTcpPort fileToGet localDirectory");
+            return;
+        }        
+        */
+        fileName = fileToGet.trim();
+        localDirectory = new File(stringLocalDirectory.trim());
         
-    }
+        if(!localDirectory.exists()){
+            System.out.println("A directoria " + localDirectory + " nao existe!");
+            return;
+        }
+        
+        if(!localDirectory.isDirectory()){
+            System.out.println("O caminho " + localDirectory + " nao se refere a uma directoria!");
+            return;
+        }
+        
+        if(!localDirectory.canWrite()){
+            System.out.println("Sem permissoes de escrita na directoria " + localDirectory);
+            return;
+        }
+        
+        try{
+            try{
+                localFilePath = localDirectory.getCanonicalPath()+File.separator+fileName;
+                localFileOutputStream = new FileOutputStream(localFilePath);
+                System.out.println("Ficheiro " + localFilePath + " criado.");
+            }catch(IOException e){
+                if(localFilePath == null){
+                    System.out.println("Ocorreu a excepcao {" + e +"} ao obter o caminho canonico para o ficheiro local!");   
+                }else{
+                    System.out.println("Ocorreu a excepcao {" + e +"} ao tentar criar o ficheiro " + localFilePath + "!");
+                }
+                return;
+            }
+
+            try{
+                /*
+                serverPort = Integer.parseInt(args[1]);
+                
+                socketToServer = new Socket(args[0], serverPort);
+                
+                socketToServer.setSoTimeout(TIMEOUT*1000);
+                */
+                in = servidorPrincipal.getInputStream();
+                pout = new PrintWriter(servidorPrincipal.getOutputStream(), true);
+                
+                pout.println(fileName);
+                pout.flush();
+
+                while((nbytes = in.read(fileChunck)) > 0){                    
+                    //System.out.println("Recebido o bloco n. " + ++contador + " com " + nbytes + " bytes.");
+                    localFileOutputStream.write(fileChunck, 0, nbytes);
+                    //System.out.println("Acrescentados " + nbytes + " bytes ao ficheiro " + localFilePath+ ".");                    
+                }                    
+                
+                System.out.println("Transferencia concluida.");
+
+            }catch(UnknownHostException e){
+                 System.out.println("Destino desconhecido:\n\t"+e);
+            }catch(NumberFormatException e){
+                System.out.println("O porto do servidor deve ser um inteiro positivo:\n\t"+e);
+            }catch(SocketTimeoutException e){
+                System.out.println("Não foi recebida qualquer bloco adicional, podendo a transferencia estar incompleta:\n\t"+e);
+            }catch(SocketException e){
+                System.out.println("Ocorreu um erro ao nÃ­vel do socket TCP:\n\t"+e);
+            }catch(IOException e){
+                System.out.println("Ocorreu um erro no acesso ao socket ou ao ficheiro local " + localFilePath +":\n\t"+e);
+            }
+            
+        }finally{
+            
+            if(socketToServer != null){
+                try {
+                    socketToServer.close();
+                } catch (IOException ex) {}
+            }
+            
+            if(localFileOutputStream != null){
+                try{
+                    localFileOutputStream.close();
+                }catch(IOException e){}
+            }
+            
+        }               
+
+   }
     
     void ligacaoServidor() {
         
